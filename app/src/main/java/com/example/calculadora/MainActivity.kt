51f2422cab3f6bbdd40402b7f35af4ebdc6cc52b
Jordi Navarro <jornavmar6@alu.edu.gva.es>
2025-10-28 23:20:41 +0100

    DECLARAMOS VARIABLES Y EMPEZAMOS DISEÑO

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -3,12 +3,27 @@ package com.example.calculadora
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.calculadora.ui.theme.CalculadoraTheme
 
-class MainActivity : ComponentActivity()
-{
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -18,15 +33,42 @@ class MainActivity : ComponentActivity()
 }
 
 @Composable
-fun CalculadoraApp()
-{
+fun CalculadoraApp() {
+    val calc = remember { Calculadora() }
+
+    var pantalla by remember { mutableStateOf("") }
+    var operador by remember { mutableStateOf<String?>(null) }
+    var num1 by remember { mutableStateOf<Double?>(null) }
+
+    //diseño
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "CALCULADORA",
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(Modifier.height(50.dp))
+
+        Text(
+            text = pantalla.ifEmpty { "0" },
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+        )
+
+    }
 
 }
 
 @Preview(showBackground = true)
 @Composable
-fun CalculadoraPreview()
-{
+fun CalculadoraPreview() {
     CalculadoraTheme {
         CalculadoraApp()
     }
