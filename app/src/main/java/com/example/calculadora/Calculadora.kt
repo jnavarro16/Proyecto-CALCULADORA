a4191341e6695048f790d3379482cf1096367bbf
Jordi Navarro <jornavmar6@alu.edu.gva.es>
2025-10-28 22:47:41 +0100

    DECLARAMOS FUNCIONES EN MainActivity.kt

--- app/src/main/java/com/example/calculadora/Calculadora.kt
+++ app/src/main/java/com/example/calculadora/Calculadora.kt
@@ -1,15 +1,13 @@
 package com.example.calculadora
 
-class Calculadora
-{
-    fun suma(a: Int, b: Int): Int = a + b
-    fun resta(a: Int, b: Int): Int = a - b
-    fun multiplicacion(a: Int, b: Int): Int = a * b
-    fun division(a: Int, b: Int): Double
-    {
-        if (b == 0) {
+class Calculadora {
+    fun suma(a: Double, b: Double): Double = a + b
+    fun resta(a: Double, b: Double): Double = a - b
+    fun multiplicacion(a: Double, b: Double): Double = a * b
+    fun division(a: Double, b: Double): Double {
+        if (b == 0.0) {
             throw IllegalArgumentException("No se puede dividir entre 0")
         }
-        return a.toDouble() / b
+        return a / b
     }
 }
--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,9 +1,34 @@
 package com.example.calculadora
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.calculadora.ui.theme.CalculadoraTheme
 
 class MainActivity : ComponentActivity()
 {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
 
+        }
+    }
+}
+
+@Composable
+fun CalculadoraApp()
+{
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CalculadoraPreview()
+{
+    CalculadoraTheme {
+        CalculadoraApp()
+    }
 }
 
